89341a7f2d4cc7838daaf0fbe3f58281fe132487
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-07-17 18:31:24 -0400

    Fix: Adjusting console.logs

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,8 @@ import axios from 'axios'
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 const apiLocalUrl = import.meta.env.VITE_API_LOCAL_URL
 
-console.log("Base URL: ", apiBaseUrl)
-console.log("Local URL: ", apiLocalUrl)
-
 const api = axios.create({
-  baseURL: apiLocalUrl // ou apiBaseUrl dependendo do ambiente
+  baseURL: apiLocalUrl
 })
 
 export default api
