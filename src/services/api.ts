ea1bb57c4680be9bbb98654c408d2d281f32705f
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-07-17 21:16:56 -0400

    Feat: adjusting baseUrl api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
-//const apiLocalUrl = import.meta.env.VITE_API_LOCAL_URL
+//const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
+const apiLocalUrl = import.meta.env.VITE_API_LOCAL_URL
 
 const api = axios.create({
-  baseURL: apiBaseUrl
+  baseURL: apiLocalUrl
 })
 
 export default api
