cd56963f9fcbddb058543762d8ebeef56bb2f690
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-07-17 20:19:59 -0400

    Feat: Changing baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 const apiLocalUrl = import.meta.env.VITE_API_LOCAL_URL
 
 const api = axios.create({
-  baseURL: apiLocalUrl
+  baseURL: apiBaseUrl
 })
 
 export default api
